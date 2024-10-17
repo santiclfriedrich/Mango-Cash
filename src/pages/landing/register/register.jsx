469218b528ff15c4327aa5fc9353fb30b0649594
santiclfriedrich <santiclear1@hotmail.com>
2024-10-16 23:28:15 -0300

    Instalacion de Redux Toolkit y creacion de registroSlice

--- src/pages/landing/register/register.jsx
+++ src/pages/landing/register/register.jsx
@@ -1,58 +1,28 @@
 import './register.css'
 import React from 'react'
-import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom'
+import { actualizarFormulario, registrarUsuario } from '../../../redux/registroSlice';
 
-import { ToastContainer, toast } from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 
     const FormularioRegistro = () => {
-        const [formData, setFormData] = useState({
-        "nombre": '',
-        "apellido": '',
-        "email": '',
-        "password": '',
-        "fechaNacimiento": '',
-        "genero": ''
-        });
+       
+        const dispatch = useDispatch();
+        const { formData, status } = useSelector((state) => state.registro )
 
-  
-    const handleChange = (e) => {
-      const { name, value } = e.target;
-      setFormData({ ...formData, [name]: value });
-    };
-  
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        console.log('Datos a enviar:', formData);
-      
-        try {
-          const response = await fetch('http://localhost:8080/api/v1/usuarios', {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(formData),
-          });
-      
-          const data = await response.json();
-      
-          // Muestra el toast dependiendo de la respuesta
-          if (response.ok) {
-            toast.success(data.message || 'Registro exitoso');
-            // Refresca la página
-            setTimeout(() => {
-                window.location.href = 'http://localhost:5173/app/dashboard';
-            }, 3000);
-          } else {
-            toast.error(data.error || 'Error al registrar el usuario');
-          }
-        } catch (error) {
-          console.error('Error al enviar el formulario:', error);
-          toast.error('Error al enviar el formulario');
+        const handleChange = (e) => {
+            const {name, value} = e.target
+            dispatch(actualizarFormulario({name, value }))
         }
-      };   
+
+        const handleSubmit = (e) => {
+            e.preventDefault()
+            dispatch(registrarUsuario(formData))
+        }
+
 
   return (
         <div className="login-container">
@@ -71,29 +41,29 @@ import 'react-toastify/dist/ReactToastify.css';
                     <div className='container-row-register'>
                         <div>
                             <label className="form-label-register" htmlFor="nombre">Nombre</label>
-                            <input className='form-input-login form-input-register' type="text" name="nombre" value={formData.nombre} onChange={handleChange}/>
+                            <input className='form-input-login form-input-register' type="text" name="nombre" value={formData.nombre || ''} onChange={handleChange}/>
                         </div>
                         <div>
                             <label className="form-label-register" htmlFor="apellido">Apellido</label>
-                            <input className='form-input-login form-input-register' type="text" name="apellido" value={formData.apellido} onChange={handleChange}/>
+                            <input className='form-input-login form-input-register' type="text" name="apellido" value={formData.apellido || ''} onChange={handleChange}/>
                         </div>
 
                     </div>
 
                     <label className="form-label-login" htmlFor="email">Direccion de e-mail</label>
-                    <input className='form-input-login' type="email" name="email" value={formData.email} onChange={handleChange}/>
+                    <input className='form-input-login' type="email" name="email" value={formData.email || ''} onChange={handleChange}/>
 
                     <label className="form-label-login" htmlFor="password">Contraseña</label>
-                    <input className='form-input-login' type="password" name="password" value={formData.password} onChange={handleChange}/>
+                    <input className='form-input-login' type="password" name="password" value={formData.password || ''} onChange={handleChange}/>
 
                     <div className='container-row-register'>
                         <div>
                             <label className="form-label-login" htmlFor="fechaNacimiento">Fecha de Nacimiento</label>
-                            <input className='form-input-login form-input-register' type="date" name="fechaNacimiento" value={formData.fechaNacimiento} onChange={handleChange}/>
+                            <input className='form-input-login form-input-register' type="date" name="fechaNacimiento" value={formData.fechaNacimiento || ''} onChange={handleChange}/>
                         </div>
                         <div>
                             <label className="form-label-login" htmlFor="genero">Género</label>
-                            <select className='form-select-register' name="genero" value={formData.genero} onChange={handleChange}>
+                            <select className='form-select-register' name="genero" value={formData.genero || ''} onChange={handleChange}>
                                 <option hidden value=""></option>
                                 <option value="M">Masculino</option>
                                 <option value="F">Femenino</option>
@@ -104,7 +74,9 @@ import 'react-toastify/dist/ReactToastify.css';
                     </div>
 
 
-                    <button className='form-button-registro' type="submit">Registrate</button>
+                    <button className='form-button-registro' type="submit" disabled={status === 'loading'}>
+                        {status === 'loading' ? 'Registrando...' : 'Registrate'}
+                    </button>
                 </form>
             </div>
 
